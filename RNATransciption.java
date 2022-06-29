c7135654847f48bc9521edf735e0ee86dad4252d
droidbg <41873024+droidbg@users.noreply.github.com>
2022-06-29 16:13:32 +0530

    Added Code RNATransciption

--- RNATransciption.java
+++ RNATransciption.java
@@ -1,1 +1,24 @@
 
+class RnaTranscription {
+    String transcribe(String dnaStrand) {
+        String rnaStrand="";
+       for(int i=0;i<dnaStrand.length();i++)
+           {
+               if(dnaStrand.charAt(i)=='A')
+               {
+                   rnaStrand+="U";
+               }
+               else if(dnaStrand.charAt(i)=='G')
+               {
+                   rnaStrand+="C";
+               } else if(dnaStrand.charAt(i)=='C')
+               {
+                   rnaStrand+="G";
+               } else if(dnaStrand.charAt(i)=='T')
+               {
+                   rnaStrand+="A";
+               }
+           }
+        return rnaStrand;
+    }
+}
